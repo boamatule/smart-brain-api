e006d7bdd3bc1bbd1f148df76cb5bc47224cd544
boa <boa.matule@gmail.com>
2021-08-14 16:46:04 +0200

    Creates new functions to handle the signinAuthentication using JWT token

--- server.js
+++ server.js
@@ -18,15 +18,17 @@ const db = knex({
 
 const app = express();
 
-app.use(morgan('combined'))
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan('combined'))
 
 app.get('/', (req, res)=> { res.send("IT'S WORKING") })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+// app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
